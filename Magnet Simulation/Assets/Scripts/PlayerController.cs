7190450a51854f5dea8df4ed2ff895d1bb32d4e7
SayedXAn <anow.sayed@gmail.com>
2025-09-22 00:55:43 +0600

    Added platforms

--- Magnet Simulation/Assets/Scripts/PlayerController.cs
+++ Magnet Simulation/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using Terresquall;
+using System.Drawing;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float moveSpeed = 4f;
     public float rotationSpeed = 10f;
     private Vector3 moveVector = Vector3.zero;
+    public Material[] mats;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -35,6 +37,16 @@ public class PlayerController : MonoBehaviour
     protected void LateUpdate()
     {
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0 );
+    }
+
+    private void ChangePolarityAndMaterial()
+    {
+        GetComponent<Renderer>().material = mats[0];
+    }
+
+    private void ActicvateSouthPole()
+    {
+        // North pole will be represented by "Red" color and the South pole by "Blue" color
     }
 }
 
